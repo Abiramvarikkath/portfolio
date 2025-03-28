238ea87d4262eb09a0603838888d943c95498d56
Abiramvarikkath <abhiramv730@gmail.com>
2025-03-28 11:30:17 +0530

    committed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-red-200 text-yellow-800">Main</div>
+      <div className="animate-bounce">Rinshad .... Shafana</div>
+      <div>
+      <button  class="bg-sky-500 hover:bg-sky-700 ...">shafana</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/base.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function base() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default base
