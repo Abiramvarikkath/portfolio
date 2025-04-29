c98945283c60cfdba7b7d32c7976df005e552381
Abiram <abhiramv730@gmail.com>
2025-04-29 14:38:16 +0530

    completed

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -7,20 +7,20 @@ function Projects() {
     <div className='grid grid-cols-3 gap-3 flex text-center text-bold mt-12 p-10 '>
         <div >
           <div className='bg-white w-70 h-60 rounded-2xl shadow-xl flex flex-col items-center transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 pt-10'>
-          <SiFlipkart className='text-yellow-400 w-20 h-20 animate-pulse hover:animate-spin'/>
+          <SiFlipkart className='text-yellow-400 w-20 h-20 animate-pulse hover:animate-spin hover:cursor-pointer'/>
           <h1 className='font-bold'>Commerce Website</h1>
           </div>
           </div>
         <div >
           <div className='bg-white  rounded-2xl shadow-xl w-70 h-60  flex flex-col items-center transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 pt-10' >
-        <SiStarbucks className="text-green-700 w-20 h-20 animate-pulse  hover:animate-spin" />
+        <SiStarbucks className="text-green-700 w-20 h-20 animate-pulse  hover:animate-spin hover:cursor-pointer" />
         <h1 className='font-bold'>StarBucks</h1>
         </div>
         </div>
         
         <div>
           <div className='bg-white object-center w-70 h-60 rounded-2xl shadow-xl flex flex-col items-center transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110  pt-10'>
-            <SiKtm className=' text-orange-600 w-24 h-24 animate-pulse  hover:animate-spin'/>
+            <SiKtm className=' text-orange-600 w-24 h-24 animate-pulse  hover:animate-spin hover:cursor-pointer'/>
             <h1 className='font-bold'>KTM</h1>
           </div>
         </div>
--- src/Skill.jsx
+++ src/Skill.jsx
@@ -8,39 +8,39 @@ function Skill() {
     <div id='Skill' className='w-full h-fit'>
       <h1 className='text-center pt-10 font-bold text-4xl'>Skill</h1>
       <div className='grid grid-cols-5 gap-10  text-1xl items-center text-center  mt-28 justify-items-center '>
-      <div className='w-40  rounded-2xl  h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl '>
+      <div className='w-40  rounded-2xl  h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl hover:cursor-pointer'>
         <AiFillHtml5 size={76} className="object-center content-center text-orange-500 animate-pulse hover:animate-spin "/>
         <h1 className='text-center'>HTML</h1>
       </div>
-      <div className='w-40  rounded-2xl  h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl'>
+      <div className='w-40  rounded-2xl  h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl hover:cursor-pointer'>
           <FaCss3Alt  size={75} className="object-center text-blue-600 animate-pulse hover:animate-spin"/>
           <h1 className='text-center'>CSS</h1>
       </div>
-      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl'>
+      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl hover:cursor-pointer'>
           <IoLogoJavascript  size={70} className="object-center text-yellow-300  animate-pulse hover:animate-spin"/>
           <h1 className='text-center'>JAWASCRIPT</h1>
       </div>
-      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl'>
-          <FaNodeJs  size={70} className="object-center text-green-500 animate-pulse hover:animate-spin"/>
+      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl hover:cursor-pointer'>
+          <FaNodeJs  size={70} className="object-center text-green-500 animate-pulse hover:animate-spin "/>
           <h1 className='text-center'>NODE.JS</h1>
       </div>
-      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl'>
+      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl hover:cursor-pointer'>
           <FaReact  size={70} className="object-center text-blue-400 animate-pulse hover:animate-spin"/>
           <h1 className='text-center'>REACT</h1>
       </div>
-      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl'>
+      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl hover:cursor-pointer'>
           <SiMongodb  size={70} className="object-center text-green-500 animate-pulse hover:animate-spin "/>
           <h1 className='text-center'>MONGODB</h1>
       </div>
-      <div className='w-40  rounded-2xl  h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl'>
+      <div className='w-40  rounded-2xl  h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl hover:cursor-pointer'>
           <SiTailwindcss  size={70} className="object-center text-blue-700 animate-pulse hover:animate-spin"/>
           <h1 className='text-center'>TAILWIND</h1>
       </div>
-      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl'>
+      <div className='w-40  rounded-2xl h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl hover:cursor-pointer'>
           <FaBootstrap  size={70} className="object-center text-violet-600 animate-pulse hover:animate-spin "/>
           <h1 className='text-center'>BOOTSTRAP</h1>
       </div>
-      <div className='w-40   h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl  rounded-2xl'>
+      <div className='w-40   h-40 place-content-center  justify-items-center shadow-md hover:shadow-2xl  rounded-2xl hover:cursor-pointer'>
           <SiExpress  size={70} className="object-center animate-pulse hover:animate-spin
  "/>
           <h1 className='text-center'>EXPRESS.JS</h1>
